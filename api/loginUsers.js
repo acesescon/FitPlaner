aded52a7d0bd23c6846f6daaec6a12d962e4dfdf
ace sescon <acesescon5@email.com>
2025-12-01 13:37:41 +0800

    again

--- api/loginUsers.js
+++ api/loginUsers.js
@@ -3,7 +3,7 @@ import { supabase } from "./database/server.js";
 
 export default async function handler(req, res) {
   try {
-    if (req.method !== "POST") {
+    if (req.method !== "GET") {
       return res.status(405).json({ error: "Method not allowed" });
     }
 
