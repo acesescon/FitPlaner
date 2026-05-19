c4ca4913e1f0052f3f31d592eee9ace8e1277dc8
acesescon <acesescon5@gmail.com>
2026-05-19 13:47:44 +0800

    add ping

--- api/ping.js
+++ api/ping.js
@@ -1,45 +1,26 @@
 import { createClient } from '@supabase/supabase-js'
 
-export default async function handler(req, res) {
-    try {
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+    )
 
-        // Check env variables
-        if (!import.meta.env.SUPABASE_URL) {
-        return res.status(500).json({
-            error: 'Missing SUPABASE_URL'
-        })
-        }
-
-        if (!import.meta.env.SUPABASE_SERVICE_ROLE_KEY) {
-        return res.status(500).json({
-            error: 'Missing SUPABASE_SERVICE_ROLE_KEY'
-        })
-        }
-
-        const supabase = createClient(
-        import.meta.env.SUPABASE_URL,
-        import.meta.env.SUPABASE_SERVICE_ROLE_KEY
-        )
-
-        const { data, error } = await supabase
+    export default async function handler(req, res) {
+    const { error } = await supabase
         .from('users')
-        .select('*')
+        .select('id')
         .limit(1)
 
-        if (error) {
+    if (error) {
         return res.status(500).json({
-            supabaseError: error.message
-        })
-        }
-
-        return res.status(200).json({
-        ok: true,
-        data
-        })
-
-    } catch (err) {
-        return res.status(500).json({
-        crash: err.message
+        ok: false,
+        error: error.message
         })
     }
+
+    res.status(200).json({
+        ok: true
+    })
+
+    console.log(res)
 }
